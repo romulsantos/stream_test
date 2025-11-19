573430f7d0a976d47db2bd6c9f38c087ecbd8934
Romulo Santos <2025089@student.cct.ie>
2025-11-19 15:16:32 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -19,4 +19,6 @@ def load_data(nrows):
 data_load_state = st.text("Loading data...")
 data = load_data(10000)
 data_load_state = st.text("Loading data...Done!")
-           
+
+st.subheader("Raw Data")
+st.write("data")
